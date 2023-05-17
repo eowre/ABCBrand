e51a0230d6f496fbf45821c7bf8f85e623dee987
eowre <evanowre12@gmail.com>
2023-05-16 20:13:54 -0400

    azure sql db

--- ABCBrandEXAPI/obj/Debug/net6.0/ABCBrandEXAPI.AssemblyInfo.cs
+++ ABCBrandEXAPI/obj/Debug/net6.0/ABCBrandEXAPI.AssemblyInfo.cs
@@ -11,6 +11,7 @@
 using System;
 using System.Reflection;
 
+[assembly: Microsoft.Extensions.Configuration.UserSecrets.UserSecretsIdAttribute("181621f0-dd12-4b38-a54b-6f3c31bd50a3")]
 [assembly: System.Reflection.AssemblyCompanyAttribute("ABCBrandEXAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
